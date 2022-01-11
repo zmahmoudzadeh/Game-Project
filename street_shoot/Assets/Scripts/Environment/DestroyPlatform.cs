622dd280f0ef4ff1e6070125f90f6ea7ac58faab
ghzamani <ghzamaninejad@gmail.com>
2022-01-11 19:02:37 +0330

    Fix random platforms

---
+++ street_shoot/Assets/Scripts/Environment/DestroyPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlatform : MonoBehaviour
+{
+	public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float camera_pos = camera.transform.position.z;
+		if (transform.position.z < camera_pos - 20)
+		{
+			Destroy(gameObject);
+		}
+    }
+}
---
+++ street_shoot/Assets/Scripts/Environment/GeneratePlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePlatform : MonoBehaviour
+{
+	//public GameObject[] section;
+	public float zPos = 40f;
+	private bool creatingSection = false;
+	public GameObject Street;
+
+
+	void Update()
+	{
+		if (creatingSection == false)
+		{
+			creatingSection = true;
+			StartCoroutine(GenerateSection());
+		}
+	}
+
+	IEnumerator GenerateSection()
+	{
+		//secNum = Random.Range(0, 10);
+		//Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+		Instantiate(Street, new Vector3(0, 0, zPos), Quaternion.identity);
+
+
+		zPos += 40;
+		yield return new WaitForSeconds(3); //remember! adjust with ball speed
+		creatingSection = false;
+	}
+}
